7bdf7fcc35359434a8f2f5a710b7c2453b1d27bd
Iryna Rozmaita <iriszechka@ukr.net>
2024-01-26 16:22:07 +0200

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,46 @@
+import { Component } from 'react';
+import Header from './Header/Header';
+import ToDoList from './ToDoList/ToDoList';
+import Modal from './Modal/Modal';
+import FormLogin from './FormLogin/FormLogin';
+import { nanoid } from 'nanoid';
+
+class App extends Component {
+  state = {
+    isShowModal: false,
+  };
+
+  showModal = () => {
+    this.setState({ isShowModal: true })
+  };
+
+  closeModal = () => {
+    this.setState({ isShowModal: false })
+	};
+	
+	createUser = (data) => {
+		//console.log(data)	
+		const newUser = {
+			...data,
+			id: nanoid(),
+		};
+		console.log(newUser)
+	};
+
+	render() {
+		return (
+			<div className='container'>
+				<Header showModal={this.showModal} />
+				<ToDoList />
+				{this.state.isShowModal && (
+					<Modal closeModal={this.closeModal}>
+						<FormLogin createUser={this.createUser}
+						closeModal={this.closeModal}/>
+					</Modal>
+				)}				
+			</div>
+		)
+	};
+};
+
+export default App
