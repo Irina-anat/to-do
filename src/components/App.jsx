145ad9f667ad73e5c109761abdf1f28d4f68ee19
Iryna Rozmaita <iriszechka@ukr.net>
2024-02-15 19:42:16 +0200

    update App + Modal / Used hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,55 +1,92 @@
-import { Component } from 'react';
+import {  useState } from 'react';
 import Header from './Header/Header';
 import ToDoList from './ToDoList/ToDoList';
 import Modal from './Modal/Modal';
 import FormLogin from './FormLogin/FormLogin';
 import { nanoid } from 'nanoid';
-import Search from './Search/Search';
-import ContentInfo from './ContentInfo/ContentInfo';
+/* import Search from './Search/Search';
+import ContentInfo from './ContentInfo/ContentInfo'; */
 
-class App extends Component {
-  state = {
-	  isShowModal: false,
-	  searchText:'',
-  };
+const App = () => {
+  const [isShowModal, SetIsShowModal] = useState(false);
+ // const [searchText, SetSearchText] = useState('');
 
-  showModal = () => {
-    this.setState({ isShowModal: true })
-  };
+  const showModal = () => SetIsShowModal(true);
 
-  closeModal = () => {
-    this.setState({ isShowModal: false })
-	};
-	
-	createUser = (data) => {
-		//console.log(data)	
-		const newUser = {
-			...data,
-			id: nanoid(),
-		};
-		console.log(newUser)
-	};
+  const closeModal = () => SetIsShowModal(false);
 
-	handleSearch = (searchText) => {
-	this.setState({searchText})	
-	}
+  function createUser(data) {
+    //console.log(data)
+    const newUser = {
+      ...data,
+      id: nanoid(),
+    };
+    console.log(newUser);
+  }
+//CORS data
+  /* const handleSearch = searchText => {
+    SetSearchText(searchText);
+  }; */
 
-	render() {
-		return (
-			<div className='container'>
-				<Header showModal={this.showModal} />
-				<ToDoList />
-				{this.state.isShowModal && (
-					<Modal closeModal={this.closeModal}>
-						<FormLogin createUser={this.createUser}
-						closeModal={this.closeModal}/>
-					</Modal>
-				)}	
-				<Search handleSearch={this.handleSearch} />
-				<ContentInfo searchText={this.state.searchText} />
-			</div>
-		)
-	};
+  return (
+    <div className="container">
+      <Header showModal={showModal} />
+      <ToDoList />
+      {isShowModal && (
+        <Modal closeModal={closeModal}>
+          <FormLogin createUser={createUser} closeModal={closeModal} />
+        </Modal>
+      )}
+      {/* <Search handleSearch={handleSearch} />
+      <ContentInfo searchText={searchText} /> */}
+    </div>
+  );
 };
 
-export default App
+export default App;
+
+// class App extends Component {
+//   state = {
+// 	  isShowModal: false,
+// 	  searchText:'',
+//   };
+
+//   showModal = () => {
+//     this.setState({ isShowModal: true })
+//   };
+
+//   closeModal = () => {
+//     this.setState({ isShowModal: false })
+// 	};
+	
+// 	createUser = (data) => {
+// 		//console.log(data)	
+// 		const newUser = {
+// 			...data,
+// 			id: nanoid(),
+// 		};
+// 		console.log(newUser)
+// 	};
+
+// 	handleSearch = (searchText) => {
+// 	this.setState({searchText})	
+// 	}
+
+// 	render() {
+// 		return (
+// 			<div className='container'>
+// 				<Header showModal={this.showModal} />
+// 				<ToDoList />
+// 				{this.state.isShowModal && (
+// 					<Modal closeModal={this.closeModal}>
+// 						<FormLogin createUser={this.createUser}
+// 						closeModal={this.closeModal}/>
+// 					</Modal>
+// 				)}	
+// 				<Search handleSearch={this.handleSearch} />
+// 				<ContentInfo searchText={this.state.searchText} />
+// 			</div>
+// 		)
+// 	};
+// };
+
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,45 +1,82 @@
-import { Component } from "react";
+import { useEffect } from 'react';
 
-class Modal extends Component{
-    state = {};
-
-    componentDidMount() {
-        window.addEventListener('keydown', this.handlePressESC)
+const Modal = ({ closeModal, children }) => {
+  // console.log('Modal useEffect run')
+  useEffect(() => {
+    const handlePressESC = e => {
+      if (e.code === 'Escape') closeModal();
     };
-    
-    componentWillUnmount() {
-        window.removeEventListener('keydown', this.handlePressESC)
-    };
+    window.addEventListener('keydown', handlePressESC);
 
-    handlePressESC = (e) => {
-        if (e.code === 'Escape')
-            this.props.closeModal()
+    return () => {
+      window.removeEventListener('keydown', handlePressESC);
     };
+  }, [closeModal]);
 
-    render() {
-    const{closeModal, children} = this.props;
-    return (
-        <div
-            className='modal fade show'
-            style={{ display: 'block', backdropFilter: 'blur(5px)' }}
-        >
-            <div className='modal-dialog'>
-                <div className='modal-content'>
-                    <div className='modal-header'>
-                        <h5 className='modal-title'> Modal</h5>
-                        <button
-                            type='button'
-                            className='btn-close'
-                            aria-label='Close'
-                            onClick={closeModal}
-                        ></button>
-                    </div>
-                    <div className='modal-body'>{children}</div>
-                </div>
-            </div>
+  return (
+    <div
+      className="modal fade show"
+      style={{ display: 'block', backdropFilter: 'blur(5px)' }}
+    >
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title"> Modal</h5>
+            <button
+              type="button"
+              className="btn-close"
+              aria-label="Close"
+              onClick={closeModal}
+            ></button>
+          </div>
+          <div className="modal-body">{children}</div>
         </div>
-    )   
-    }
+      </div>
+    </div>
+  );
 };
 
 export default Modal;
+
+// class
+// class Modal extends Component{
+//     state = {};
+
+//     componentDidMount() {
+//         window.addEventListener('keydown', this.handlePressESC)
+//     };
+
+//     componentWillUnmount() {
+//         window.removeEventListener('keydown', this.handlePressESC)
+//     };
+
+//     handlePressESC = (e) => {
+//         if (e.code === 'Escape')
+//             this.props.closeModal()
+//     };
+
+//     render() {
+//     const{closeModal, children} = this.props;
+//     return (
+//         <div
+//             className='modal fade show'
+//             style={{ display: 'block', backdropFilter: 'blur(5px)' }}
+//         >
+//             <div className='modal-dialog'>
+//                 <div className='modal-content'>
+//                     <div className='modal-header'>
+//                         <h5 className='modal-title'> Modal</h5>
+//                         <button
+//                             type='button'
+//                             className='btn-close'
+//                             aria-label='Close'
+//                             onClick={closeModal}
+//                         ></button>
+//                     </div>
+//                     <div className='modal-body'>{children}</div>
+//                 </div>
+//             </div>
+//         </div>
+//     )
+//     }
+// };
