f2db6feb4b0391541a1f64007b6fbe3948850a17
Iryna Rozmaita <iriszechka@ukr.net>
2024-02-19 17:18:49 +0200

    update FormToDo used useState

--- src/components/FormToDo/FormToDo.jsx
+++ src/components/FormToDo/FormToDo.jsx
@@ -1,44 +1,83 @@
-const { Component } = require('react');
+//const { Component } = require('react');
 
-class FormToDo extends Component {
-    state = {
-        todo: ''
-    };
-    
-    handleChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
+import React, { useState } from 'react';
+
+const FormToDo = ({ addToDo }) => {
+  const [todo, setTodo] = useState('');
+
+  const handleChange = (e) => {
+    setTodo(e.target.value);
   };
 
-    handleSubmit = e => {
-        e.preventDefault();
-        this.props.addToDo(this.state.todo)
-        this.setState({
-            todo: '',
-        });
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addToDo(todo);
+    setTodo('');
+  };
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <div className="mb-3">
-          <label htmlFor="exampleInput" className="form-label">
-            Create to-do
-          </label>
-          <input
-            name="todo"
-            type="text"
-            className="form-control"
-            id="exampleInput"
-            onChange={this.handleChange}
-            value={this.state.todo}
-          />
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Add to-do
-        </button>
-      </form>
-    );
-  }
-}
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="mb-3">
+        <label htmlFor="exampleInput" className="form-label">
+          Create to-do
+        </label>
+        <input
+          name="todo"
+          type="text"
+          className="form-control"
+          id="exampleInput"
+          onChange={handleChange}
+          value={todo}
+        />
+      </div>
+      <button type="submit" className="btn btn-primary">
+        Add to-do
+      </button>
+    </form>
+  );
+};
 
 export default FormToDo;
+
+// class FormToDo extends Component {
+//     state = {
+//         todo: ''
+//     };
+    
+//     handleChange = e => {
+//     this.setState({ [e.target.name]: e.target.value });
+//   };
+
+//     handleSubmit = e => {
+//         e.preventDefault();
+//         this.props.addToDo(this.state.todo)
+//         this.setState({
+//             todo: '',
+//         });
+//     };
+
+//   render() {
+//     return (
+//       <form onSubmit={this.handleSubmit}>
+//         <div className="mb-3">
+//           <label htmlFor="exampleInput" className="form-label">
+//             Create to-do
+//           </label>
+//           <input
+//             name="todo"
+//             type="text"
+//             className="form-control"
+//             id="exampleInput"
+//             onChange={this.handleChange}
+//             value={this.state.todo}
+//           />
+//         </div>
+//         <button type="submit" className="btn btn-primary">
+//           Add to-do
+//         </button>
+//       </form>
+//     );
+//   }
+// }
+
+
